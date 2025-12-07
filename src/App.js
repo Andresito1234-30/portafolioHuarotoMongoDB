887eb041a4663011d8eaa467cc2f4a98c6ef13d4
Andresito1234-30 <2411010094@undc.edu.pe>
2025-12-06 19:04:05 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React, { useState, useEffect } from "react";
-import Preloader from "../src/components/Pre";
+import Preloader from "./components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Login from "./components/Auth/Login";
 import Signup from "./components/Auth/Signup";
-import Footer from "./components/Footer"; 
+import Footer from "./components/Footer";
 import ProtectedRoute from "./components/Auth/ProtectedRoute";
 import { AuthProvider } from "./hooks/useAuth";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate
-} from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
@@ -27,28 +22,31 @@ function App() {
     const timer = setTimeout(() => {
       upadateLoad(false);
     }, 1200);
-
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <AuthProvider>
-      <Router>
-        <Preloader load={load} />
-        <div className="App" id={load ? "no-scroll" : "scroll"}>
-          <Navbar />
-          <ScrollToTop />
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/" element={<ProtectedRoute element={<Home />} />} />
-            <Route path="/project" element={<ProtectedRoute element={<Projects />} />} />
-            <Route path="/about" element={<ProtectedRoute element={<About />} />} />
-            <Route path="*" element={<Navigate to="/login"/>} />
-          </Routes>
-          <Footer />
-        </div>
-      </Router>
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          {/* Rutas públicas */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+
+          {/* Rutas privadas */}
+          <Route path="/" element={<ProtectedRoute element={<Home />} />} />
+          <Route path="/about" element={<ProtectedRoute element={<About />} />} />
+          <Route path="/project" element={<ProtectedRoute element={<Projects />} />} />
+
+          {/* Si no existe ruta */}
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+
+        <Footer />
+      </div>
     </AuthProvider>
   );
 }
